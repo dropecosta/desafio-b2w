5396087473a4aca1495b8487ba9dbf41e10826e4
Pedro Costa <dropecosta@gmail.com>
2020-02-04 02:31:26 -0300

    fixing test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders loading', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText('Next');
+  const linkElement = getByText('loading');
   expect(linkElement).toBeInTheDocument();
 });
